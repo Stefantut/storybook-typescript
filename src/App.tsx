0f997cb9c54adc16f8f1c4b0430ec71295dee3dc
Stefan Tut <hello@stefantut.dev>
2022-10-02 00:32:36 +0100

    Created grid component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
 import React from "react";
+import Grid from "./components/Grid/Grid";
 import Box from "./components/Box/Box";
 
 function App() {
   return (
     <div className="App">
       <p>Aaapppppppppp</p>
-      <Box>box 1</Box>
+      <Grid container>
+        <Grid item>
+          <Box>box 1</Box>
+        </Grid>
+        <Grid item>
+          <Box>box 2</Box>
+        </Grid>
+        <Grid item>
+          <Box>box 3</Box>
+        </Grid>
+      </Grid>
     </div>
   );
 }
