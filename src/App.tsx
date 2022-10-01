90ed952873e88f7ada361c9804fab0704d21aabf
Stefan Tut <hello@stefantut.dev>
2022-10-02 00:22:18 +0100

    Fixed scss module imports - ts declarations

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import React from "react";
+import Box from "./components/Box/Box";
 
 function App() {
-  return <div className="App">aa pppppppppp</div>;
+  return (
+    <div className="App">
+      <p>Aaapppppppppp</p>
+      <Box>box 1</Box>
+    </div>
+  );
 }
 
 export default App;
