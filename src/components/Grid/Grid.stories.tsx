27708036275c54022dea93ca8a7a49be47c184df
Stefan Tut <hello@stefantut.dev>
2022-10-02 02:20:22 +0100

    Displayed with ruller component

--- src/components/Grid/Grid.stories.tsx
+++ src/components/Grid/Grid.stories.tsx
@@ -1,7 +1,7 @@
-import React from "react";
 import { ComponentStory, ComponentMeta } from "@storybook/react";
 import Grid from "./Grid";
 import Box from "../Box/Box";
+import GridRuler from "../GridRuler/GridRuler";
 export default {
   title: "Grid",
   component: Grid,
@@ -18,6 +18,33 @@ export const Basic: ComponentStory<any> = (args) => (
       {...args}
     >
       <Grid item cols={1}>
+        <Box>box 1</Box>
+      </Grid>
+      <Grid item cols={1}>
+        <Box>box 2</Box>
+      </Grid>
+      <Grid item cols={2}>
+        <Box>box 3</Box>
+      </Grid>
+      <Grid item cols={1}>
+        <Box>box 4</Box>
+      </Grid>
+    </Grid>
+  </div>
+);
+
+export const WithRuler: ComponentStory<any> = (args) => (
+  <div style={{ position: "relative" }}>
+    <GridRuler spacing="sm" />
+    <Grid
+      container
+      spacing="lg"
+      justifyContent="space-between"
+      alignItems="center"
+      style={{ height: "100%" }}
+      {...args}
+    >
+      <Grid item cols={2}>
         <Box>box 1</Box>
       </Grid>
       <Grid item cols={1}>
