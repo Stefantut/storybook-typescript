7065c9e57d30d86151a5a2e85abf95a8b4df2cae
Stefan Tut <hello@stefantut.dev>
2022-10-02 01:48:45 +0100

    Added justify content functionality

--- src/App.tsx
+++ src/App.tsx
@@ -5,18 +5,18 @@ import GridRuler from "./components/GridRuler/GridRuler";
 function App() {
   return (
     <div className="App" style={{ margin: "14px", position: "relative" }}>
-      <GridRuler spacing="sm" />
-      <Grid container spacing="lg">
-        <Grid item cols={6}>
+      {/* <GridRuler spacing="sm" /> */}
+      <Grid container spacing="lg" justifyContent="space-between">
+        <Grid item cols={1}>
           <Box>box 1</Box>
         </Grid>
-        <Grid item cols={6}>
+        <Grid item cols={1}>
           <Box>box 2</Box>
         </Grid>
-        <Grid item cols={6}>
+        <Grid item cols={2}>
           <Box>box 3</Box>
         </Grid>
-        <Grid item cols={6}>
+        <Grid item cols={1}>
           <Box>box 4</Box>
         </Grid>
       </Grid>
--- src/components/Grid/Grid.tsx
+++ src/components/Grid/Grid.tsx
@@ -4,6 +4,7 @@ import cn from "classnames";
 
 type Cols = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12;
 type Spacing = "sm" | "md" | "lg";
+type JustifyContent = "flex-start" | "center" | "flex-end" | "space-between";
 
 const Grid: FunctionComponent<{
   children: ReactNode;
@@ -11,12 +12,14 @@ const Grid: FunctionComponent<{
   item?: boolean;
   cols?: Cols;
   spacing?: Spacing;
-}> = ({ children, container, item, cols, spacing }) => {
+  justifyContent?: JustifyContent;
+}> = ({ children, container, item, cols, spacing, justifyContent }) => {
   const classNames = cn({
     [styles.Grid_container]: container,
     [styles.Grid_item]: item,
     [styles[`Grid_cols_${cols}`]]: cols,
     [styles[`Grid_spacing_${spacing}`]]: spacing,
+    [styles[`Grid_justifyContent_${justifyContent}`]]: justifyContent,
   });
   return <div className={classNames}>{children}</div>;
 };
